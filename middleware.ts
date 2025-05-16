06a3539afc139d854e53de89248f98b35a9aba46
Axel Huerta <axelhuerta.dev@gmail.com>
2025-05-15 19:37:37 -0600

    fix redirect error

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,19 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   const session = request.cookies.get("PHPSESSID")?.value;
 
+  const url = new URL(request.url);
+  const baseUrl = `${url.protocol}//${url.host}`;
+
   if (!session && request.nextUrl.pathname !== "/login") {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/login", baseUrl), {
+      status: 302,
+    });
   }
 
   if (session && request.nextUrl.pathname === "/login") {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/", baseUrl), {
+      status: 302,
+    });
   }
 
   return NextResponse.next();
